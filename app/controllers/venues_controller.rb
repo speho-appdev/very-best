35bb42419b4ee4359b9f3a7b99a3e8856260c6d7
Jenna McKinney <jenjenmckinney@uchicago.edu>
2018-03-12 15:05:02 +0000

    updated

--- app/controllers/venues_controller.rb
+++ app/controllers/venues_controller.rb
@@ -2,6 +2,7 @@ class VenuesController < ApplicationController
   def index
     @q = Venue.ransack(params[:q])
     @venues = @q.result(:distinct => true).includes(:bookmarks, :neighborhood, :fans, :specialties).page(params[:page]).per(10)
+    
     @location_hash = Gmaps4rails.build_markers(@venues.where.not(:address_latitude => nil)) do |venue, marker|
       marker.lat venue.address_latitude
       marker.lng venue.address_longitude
