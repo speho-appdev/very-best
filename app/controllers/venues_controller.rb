d86228dc603a25244fea77a22099aeffca9c524a
Spencer Ho <speho@uchicago.edu>
2018-03-13 22:11:28 +0000

    venue
    venue functionality done

--- app/controllers/venues_controller.rb
+++ app/controllers/venues_controller.rb
@@ -1,8 +1,9 @@
 class VenuesController < ApplicationController
   def index
-    @q = Venue.ransack(params[:q])
-    @venues = @q.result(:distinct => true).includes(:bookmarks, :neighborhood, :fans, :specialties).page(params[:page]).per(10)
-    
+    @q = current_user.bookmarked_venues.ransack(params[:q])
+    #@q = Venue.ransack(params[:q])
+    #@venues = @q.result(:distinct => true).includes(:bookmarks, :neighborhood, :fans, :specialties).page(params[:page]).per(10)
+    @venues = @q.result.includes(:bookmarks, :neighborhood, :fans, :specialties).page(params[:page]).per(10)
     @location_hash = Gmaps4rails.build_markers(@venues.where.not(:address_latitude => nil)) do |venue, marker|
       marker.lat venue.address_latitude
       marker.lng venue.address_longitude
